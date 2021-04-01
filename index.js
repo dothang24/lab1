617cc20307837ba81dea15f89eec51c93a0220b3
dothang24 <80875393+dothang24@users.noreply.github.com>
2021-04-01 18:36:46 +0700

    update

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ let http = require('http');
 let server = http.Server(app);
 
 app.use(express.static('client'));
-
+app.use('/css',express.static(__dirname + 'client/css'));
+app.use('/images',express.static(__dirname + 'client/images'));
+app.use('/js',express.static(__dirname + 'client/js'));
 server.listen(PORT, function() {
     console.log('DoThang is Running');
 });
