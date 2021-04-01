2433a42c492f7470d722f262454052f5065589b7
dothang24 <80875393+dothang24@users.noreply.github.com>
2021-04-01 12:19:19 +0700

    first commit

---
+++ client/index.js
@@ -0,0 +1,12 @@
+let PORT = process.env.PORT || 5000;
+let express = require('express');
+let app = express();
+
+let http = require('http');
+let server = http.Server(app);
+
+app.use(express.static('client'));
+
+server.listen(PORT, function() {
+    console.log('DoThang is Running');
+});
