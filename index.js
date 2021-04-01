6ebbd573f90d6cd7828ee494ec39222f4a6bbeb2
dothang24 <80875393+dothang24@users.noreply.github.com>
2021-04-01 18:12:16 +0700

    abc

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
-// let PORT = process.env.PORT || 5000;
-// let express = require('express');
-// let app = express();
+let PORT = process.env.PORT || 5000;
+let express = require('express');
+let app = express();
 
-// let http = require('http');
-// let server = http.Server(app);
+let http = require('http');
+let server = http.Server(app);
 
-// app.use(express.static('client'));
+app.use(express.static('client'));
 
-// server.listen(PORT, function() {
-//     console.log('DoThang is Running');
-// });
-
-var express = require('express');
-var exphbs  = require('express-handlebars');
-
-var app = express();
-
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-
-app.get('/', function (req, res) {
-    res.render('client');
+server.listen(PORT, function() {
+    console.log('DoThang is Running');
 });
 
-app.listen(3000);
